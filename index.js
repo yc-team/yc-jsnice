a93761a2728e04d0935568de0e42be512887e997
zhangyaochun <zhangyaochun@wandoujia.com>
2014-09-07 15:51:48 +0800

    fix feature

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 //core
 var fs = require('fs');
+var path = require('path');
 
 //thire-part
 var request = require('request');
@@ -9,10 +10,37 @@ var request = require('request');
 //TODO we will support more beautify server urls
 var URLS =  require('./lib/urls.json'); 
 
+
+function exists() {
+    var filepath = path.join.apply(path, arguments);
+    return fs.existsSync(filepath);
+}
+
+
+function isUrl(value) {
+    return /^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(value);
+}
+
 module.exports = function(data){
 
-    //support http url and local single file
-    var data = data || '';
+    //TODO support url js
+
+    //support local single file
+    var filepath = path.join(process.cwd(), data);
+
+    if (!exists(filepath)) {
+        console.log('arguments type error'.red);
+        console.log(filepath + ' is not exit'.red);
+        process.exit(-1);
+    }
+
+    if (fs.statSync(filepath).isFile()) {
+        data = fs.readFileSync(filepath, 'utf-8');
+    } else if (fs.statSync(filepath).isDirectory()) {
+        console.log('arguments type error'.red);
+        console.log(filepath + ' is not directory'.red);
+        process.exit(-1);
+    }
 
     //TODO options
     var options = {
